177d230a3dc4c09288d175728847f2d444e0e766
Riccardo Mastellone <riccardo.mastellone@gmail.com>
2014-03-18 15:10:31 +0100

    index for test

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
+// Debug
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require_once 'vendor/autoload.php';
 require_once 'Queue.php';
 
+// You may want to rewrite this function
 function log_message($T, $M) {
     // Do something
 }
 
-$j = new Queue();
-//echo $j->create('maincontroller', 'funzione', array('culo'), 'un job di test');
-echo $j->worker(10);
+//$j = new Queue();
+//echo $j->create('testcontroller', 'index', array('paramtest'), 'Testing the Library');
+//echo $j->worker();
